38bca6928058161ebc136a8e610f362fdc2ba345
mona <rostami.m4813@gmail.com>
2025-10-29 13:16:59 -0700

    Add about page

--- src/commponents/About.jsx
+++ src/commponents/About.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-     
+    <div className=' w-full h-full  flex justify-center items-center flex-col'>
+     <div className='w-[60vw] rounded-lg bg-gray-200 my-40 p-5'>
+      <h1 className='text-red-700 font-bold text-2xl py-2 text-center'>About Byzon</h1>
+      <p>Welcome to <span className=' text-red-600'>Byzon</span>your one-stop destinaton for latest and greatest in electronics from cutting-edge gadgets</p>
+      <h2 className="text-red-700  py-2">Our Mission</h2>
+      <p className=' py-2'> At Byzon our mission is to make innovative technology accessble to everyone we are passionate about connecting peaple with</p>
+      <h2 className='text-red-700'>Why Choose Byzon?</h2>
+      <ul className=' py-2'>
+        <li>top quality electronic products from trusted brands</li>
+        <li>Lightning fast and secure shipping</li>
+        <li>reliable cuuug Shopping exper experience</li>
+        <li> Easy returns and hassle free shoppping exprience</li>
+      </ul>
+      <h2 className='text-red-700  py-2'>Our Vision</h2>
+      <p> we envision a where technology elevates everyday committed to slaying ahead of the curve offerning</p>
+      <h2 className='text-red-700  py-2'>Join the Byzon Family</h2>
+      <p>wether you're a tech a peofessionall or just locking for something cool and functional Byzon</p>
+      <div className='  my-5 flex justify-center'>      <button className='bg-red-700 p-2 text-center'>Start Shopping</button>
+</div>
+     </div>
     </div>
   )
 }
